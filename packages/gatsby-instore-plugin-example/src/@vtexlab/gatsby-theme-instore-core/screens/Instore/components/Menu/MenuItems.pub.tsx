e59b5819318d77c10e75556076c86240778abfae
Guilherme Bruzzi <guibruzzi@gmail.com>
2021-04-16 16:48:40 -0300

    Add new plugins version

--- packages/gatsby-instore-plugin-example/src/@vtexlab/gatsby-theme-instore-core/screens/Instore/components/Menu/MenuItems.pub.tsx
+++ packages/gatsby-instore-plugin-example/src/@vtexlab/gatsby-theme-instore-core/screens/Instore/components/Menu/MenuItems.pub.tsx
@@ -3,8 +3,7 @@ import type { FC } from 'react'
 import { MenuItems as BaseMenuItems } from '@vtexlab/gatsby-theme-instore-core/src/screens/Instore/components/Menu/MenuItems.pub'
 import type { MenuItemType } from '@vtexlab/gatsby-theme-instore-core/src/screens/Instore/components/Menu/MenuItem.pub'
 import { MenuItemWrapper } from '@vtexlab/gatsby-theme-instore-core/src/screens/Instore/components/Menu/MenuItem.pub'
-
-import { AboutMenuItem } from './AboutMenuItem.single.pub'
+import { AboutMenuItem } from '@vtexlab/gatsby-theme-instore-poc/src/screens/Instore/components/Menu/AboutMenuItem.single.pub'
 
 export const AnotherAboutMenuItem: MenuItemType = ({
   children = 'Another about link',
